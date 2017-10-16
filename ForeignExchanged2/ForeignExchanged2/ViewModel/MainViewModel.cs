758ce9d2bb1a9fd59e8c5565c05c78e50f677b8d
joelarias2508 <joelarias2508@gmail.com>
2017-10-16 16:29:40 -0700

    The other properties and nugets

--- ForeignExchanged2/ForeignExchanged2/ViewModel/MainViewModel.cs
+++ ForeignExchanged2/ForeignExchanged2/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,17 @@ using System.Windows.Input;
 
 namespace ForeignExchanged2.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Attribute
+        private bool isRunning;
+        public bool isEnabled;
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
         #region Properties
         public Rates SourceRate { get; set; }
         public Rates TargerRate { get; set; }
@@ -19,14 +29,55 @@ namespace ForeignExchanged2.ViewModel
 
         public string Result { get; set; }
         public string Amount { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsEnabled { get; set; }
+
+        public bool IsRunning
+        { 
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+            get
+            {
+                return isRunning;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+            get
+            {
+                return isEnabled;
+            }
+        }
         #endregion
 
         public MainViewModel()
         {
             Rates = new ObservableCollection<Rates>();
+            LoadRates();
         }
+
+        
+
+        #region Methods
+        private void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading rates...";
+        } 
+        #endregion
 
         #region Command
         public ICommand ConvertsCommand { get { return new RelayCommand(Converts); } }
